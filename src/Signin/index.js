9e77a31d4d3c16603bf848f4179cae7675aa3b96
Raoni Demarchi <raonidemarchi@gmail.com>
2019-02-12 16:33:40 -0200

    Adiciona fontes e página de login

--- src/Signin/index.js
+++ src/Signin/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StatusBar, StyleSheet } from 'react-native';
+import { View, Text, StatusBar, TextInput } from 'react-native';
 import styles from './styles';
 
 class Signin extends Component {
@@ -8,7 +8,17 @@ class Signin extends Component {
       <View style={styles.view}>
         <StatusBar backgroundColor="white" barStyle="dark-content" />
 
-        <Text style={styles.title}>Signin</Text>
+        <Text style={styles.title}>Fazer login</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Seu email corporativo"
+        />
+
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+        />
       </View>
     );
   }
--- src/Signin/styles.js
+++ src/Signin/styles.js
@@ -11,6 +11,17 @@ const styles = StyleSheet.create({
     color: '#666',
     fontSize: 26,
     textAlign: 'center',
+    fontFamily: 'Nunito-Regular',
+  },
+
+  input: {
+    height: 42,
+    borderColor: '#d5d5d5',
+    borderWidth: 1,
+    borderRadius: 20,
+    width: '80%',
+    marginTop: 20,
+    paddingHorizontal: 16,
   },
 });
 
